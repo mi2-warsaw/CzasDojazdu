3d050df5f97cbf8055403e5826499b576cfebb3f
Tomasz Mikołajczyk <user@users-MacBook-Pro.local>
2016-02-14 01:49:03 +0100

    retry

--- Rscripts/ulice/slownik_ulic_alternatywny.R
+++ Rscripts/ulice/slownik_ulic_alternatywny.R
@@ -2,3 +2,4 @@ library(jsonlite)
 key <- "8c923598-2106-4c7c-9fe4-5741b6e88568"
 adres <- "https://api.um.warszawa.pl/api/action/wfsstore_get?id=8c05e43a-504d-4680-bb75-e240858aad5c&apikey="
 lista <- fromJSON(paste0(adres, key))
+
