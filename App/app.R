5b6eb3cdb9642786d11b24782e6eab9370cd20b8
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2016-04-18 22:56:44 +0200

    google analytics do body #10

--- App/app.R
+++ App/app.R
@@ -41,16 +41,16 @@ source('elements/dashboardHeader.R')
 source('elements/dashboardSidebar.R')
  
 ui <- dashboardPage(
-  tags$head(tags$script("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  skin = "black",
+  dashboardHeader,
+  dashboardSidebar,
+  dashboardBody(
+      tags$head(tags$script("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-76544868-1', 'auto');
   ga('send', 'pageview');")),
-  skin = "black",
-  dashboardHeader,
-  dashboardSidebar,
-  dashboardBody(
     tabItems(
       tabItem(
         "pokoje",
