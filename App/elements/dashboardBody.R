77dc92234272f92ba9c3d7bb8215827974728d83
abrodecka <aleksandrabrodecka@gmail.com>
2016-04-25 23:18:40 +0200

    Added files via upload

--- App/elements/dashboardBody.R
+++ App/elements/dashboardBody.R
@@ -37,7 +37,30 @@ dashboardBody <- dashboardBody(
             HTML('<p style="font-size:32px; font-family:Verdana;"align="justify"><h2> O projekcie Czas Dojazdu</h2></p>'),
             HTML('<p style="font-size:20px; font-family:Verdana;"align="justify">&nbsp;&nbsp;&nbsp;&nbsp;Aplikacja Czas Dojazdu umożliwia wyszukanie pokoi do wynajęcia, których czas dojazdu nie przekracza danych parametrów od wybranego miejsca. Informacje o pokojach pobierane są z popularnych portali z ogłoszeniami.</p>'),
            
-            HTML('<img src="/czas.jpg" alt="Opis" style="width:304px;height:228px;>'),
+            HTML('<img src="/lokalizacja.jpg" alt="Opis" width="140" height="80" align = "right" >'),
+            HTML('<p style="font-size:15px; font-family:Verdana;"align="justify">&nbsp;&nbsp;&nbsp;&nbsp; 1) Podaj lokalizację w Warszawie, do której chciałbyś dojechać.</p>'),
+            
+            HTML('<img src="/transport.jpg" alt="Opis" width="140" height="100" align = "left" >'),
+            HTML('<p style="font-size:15px; font-family:Verdana;"align="justify">&nbsp;&nbsp;&nbsp;&nbsp; 2) Wybierz środek transportu.  Umożliwiamy 3 opcję: samochód,  rower oraz pieszo.</p>'),
+            
+            HTML('<img src="/czas.jpg" alt="Opis" width="140" height="60" align = "right" >'),
+            HTML('<p style="font-size:15px; font-family:Verdana;"align="justify">&nbsp;&nbsp;&nbsp;&nbsp; 3) Określ maksymalny czas dojazdu (w minutach).</p>'),
+            
+            HTML('<img src="/cena.jpg" alt="Opis" width="140" height="60" align = "right" >'),
+            HTML('<p style="font-size:15px; font-family:Verdana;"align="justify">&nbsp;&nbsp;&nbsp;&nbsp; 4) Podaj zakres cenowy .</p>'),
+            
+            HTML('<img src="/data.jpg" alt="Opis" width="140" height="60" align = "left" >'),
+            HTML('<p style="font-size:15px; font-family:Verdana;"align="justify">&nbsp;&nbsp;&nbsp;&nbsp; 5) Sprecyzuj datę ogłoszeń. (Sugerujemy ostatnie 2-3 dni, ze względu na aktualność)</p>'),
+            
+            HTML('<img src="/pokaz.jpg" alt="Opis" width="100" height="50" align = "right" >'),
+            HTML('<p style="font-size:15px; font-family:Verdana;"align="justify">&nbsp;&nbsp;&nbsp;&nbsp;  6) Następnie kliknij w poniższy przycisk :) </p>'),
+            
+            HTML('<img src="/mapa.jpg" alt="Opis" width="200" height="100" align = "left" >'),
+            HTML('<p style="font-size:15px; font-family:Verdana;"align="justify">&nbsp;&nbsp;&nbsp;&nbsp;  7) Dostaniesz następujący wynik: Zielony marker oznacza wskazaną przez Ciebie lokalizację. Niebieski – oferty spełniające Twoje oczekiwania. </p>'),
+            
+            HTML('<img src="/dokladnie.jpg" alt="Opis" width="140" height="80" align = "right" >'),
+            HTML('<p style="font-size:15px; font-family:Verdana;"align="justify">&nbsp;&nbsp;&nbsp;&nbsp;  8) Po kliknięciu na marker otrzymasz szczegółowe informacje odnośnie oferty:  adres (wraz z linkiem  do ogłoszenia), cenę oraz wielkość.  </p>'),
+            
             
             HTML('<p style="font-size:20px; font-family:Verdana;"align="justify">&nbsp;&nbsp;&nbsp;&nbsp;Jeżeli uważasz, że aplikację można poprawić, albo masz jakiekolwiek pytania bądź sugestie, proszę zostaw wiadomość w poniższym panelu.</p>'),
             style = 'max-width:1000px;width:98%;margin:auto;'),
