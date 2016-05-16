7c278382c35a0ff614e935231195b52ad3dc492b
Marcin Kosiński <m.p.kosinski@gmail.com>
2016-05-16 15:32:24 +0200

    typo in dockerfile

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 docker run --rm -v /srv/shiny-server/CzasDojazdu/dane:/app/dane/ \
-      -t czas-dojazdu-docker-scrapuj 1 > /srv/shiny-server/CzasDojazdu/runme_out
+      -t czas-dojazdu-docker-scrapuj > /srv/shiny-server/CzasDojazdu/runme_out
 touch /srv/shiny-server/CzasDojazdu/App/app.R
 
