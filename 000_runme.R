fa15701f0abf322087f5a35e06ffbe053b4177c0
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2016-06-07 09:05:10 +0200

    #29 nowy skrypt

--- 000_runme.R
+++ 000_runme.R
@@ -1,6 +1,7 @@
 # scrapuj dane i aktualizuj baze danych
 source('Rscripts/001_pakiety.R', echo = TRUE)
-source('Rscripts/collect_data/002_gumtree_scraping.R', echo = TRUE)
-#source('Rscripts/collect_data/003_olx_scraping.R', echo = TRUE)
+source('Rscripts/collect_data/0020_gumtree_scraping_pokoje.R', echo = TRUE)
+source('Rscripts/collect_data/0021_gumtree_scraping_mieszkania.R', echo = TRUE)
+source('Rscripts/collect_data/0030_olx_scraping.R', echo = TRUE)
 # aktualizuj dane na ktorych pracuje aplikacja
 #source('Rscripts/collect_data/004_dane_pod_aplikacje.R')
