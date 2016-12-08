9eee88e1db484e7884052ac6c7a4f30b40e1076c
krzyslom <krzysztofslomczynski@gmail.com>
2016-12-08 14:56:11 +0100

    Correct path

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 docker run --rm -v /srv/shiny-server/CzasDojazdu/dane:/app/dane/ \
       -t czas-dojazdu-docker-scrapuj > /srv/shiny-server/CzasDojazdu/runme_out
-touch /srv/shiny-server/CzasDojazdu/App/app.R
-
+touch /srv/shiny-server/CzasDojazdu/pl/app.R
+touch /srv/shiny-server/CzasDojazdu/en/app.R
